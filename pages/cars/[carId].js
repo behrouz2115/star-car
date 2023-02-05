89fe6394403a5c616b9ef09e69a8a7b796f6c133
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-02-05 15:58:47 +0900

    details

--- pages/cars/[carId].js
+++ pages/cars/[carId].js
@@ -1,4 +1,11 @@
+import carsData from "../../data/dataCar";
+import { useRouter } from "next/router";
+
 const CarDetails = () => {
+  const router = useRouter();
+  const { carId } = router.query;
+  const details = carsData[carId - 1];
+  console.log(details.name);
   return <div>CarDetails</div>;
 };
 
