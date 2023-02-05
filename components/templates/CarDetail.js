5df739b963e74a2642222177dfbb7d1f182ca42f
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-02-05 18:17:25 +0900

    pretter

--- components/templates/CarDetail.js
+++ components/templates/CarDetail.js
@@ -19,52 +19,55 @@ const CarDetail = (deta) => {
     category,
   } = deta;
   console.log(name);
-  return <div className={styles.container}>
-    <img className={styles.image} src={image} />
-    <h3 className={styles.header}>{name} {model}</h3>
-    <div className={styles.details}>
-      <div>
-        <Company />
-        <p>Company</p>
-        <span>{name}</span>
+  return (
+    <div className={styles.container}>
+      <img className={styles.image} src={image} />
+      <h3 className={styles.header}>
+        {name} {model}
+      </h3>
+      <div className={styles.details}>
+        <div>
+          <Company />
+          <p>Company</p>
+          <span>{name}</span>
+        </div>
+        <div>
+          <Model />
+          <p>Model</p>
+          <span>{model}</span>
+        </div>
+        <div>
+          <Calender />
+          <p>First registration</p>
+          <span>{year}</span>
+        </div>
+        <div>
+          <Road />
+          <p>kms driven</p>
+          <span>{distance}</span>
+        </div>
       </div>
-      <div>
-        <Model />
-        <p>Model</p>
-        <span>{ model }</span>
+      <div className={styles.details}>
+        <div>
+          <Location />
+          <p>Location</p>
+          <span>{location}</span>
+        </div>
       </div>
-      <div>
-        <Calender />
-        <p>First registration</p>
-        <span>{ year }</span>
-      </div>
-      <div>
-        <Road />
-        <p>kms driven</p>
-        <span>{ distance }</span>
-      </div>
-    </div>
-    <div className={styles.details}>
-      <div>
-        <Location />
-        <p>Location</p>
-        <span>{ location }</span>
-      </div>
-    </div>
-    <div className={styles.details}>
-    <p className={styles.descriptionTitle}>Extra Information</p>
+      <div className={styles.details}>
+        <p className={styles.descriptionTitle}>Extra Information</p>
         <p className={styles.descriptionText}>{description}</p>
-    </div>
-    <div className={styles.details}>
-      <div className={styles.price}>
-      <Money />
-      <p>Price:</p>
-      <span>{ price }</span>
       </div>
+      <div className={styles.details}>
+        <div className={styles.price}>
+          <Money />
+          <p>Price:</p>
+          <span>{price}</span>
+        </div>
+      </div>
+      <button className={styles.button}>buy</button>
     </div>
-    <button className={ styles.button }>buy</button>
-  </div>;
+  );
 };
 
 export default CarDetail;
-
