6f00c64f45b78a2092d65860c94cd5e299da4bae
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-02-06 19:07:51 +0900

    sedan cars page

--- pages/categories/sedan.js
+++ pages/categories/sedan.js
@@ -1,10 +1,13 @@
-
+import carsData from "@/data/dataCar";
+import CarsList from "@/components/templates/CarsList";
 const Sedan = () => {
-    return (
-        <div>
-            Sedan
-        </div>
-    );
+  const sedancars = carsData.filter((car) => car.category === "sedan");
+  console.log(sedancars);
+  return (
+    <div>
+      <CarsList data={sedancars} />
+    </div>
+  );
 };
 
 export default Sedan;
