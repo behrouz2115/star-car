4b013938184138f2ee5f396fcd88c38f4246b047
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-02-06 18:58:23 +0900

    suv cars page

--- pages/categories/sport.js
+++ pages/categories/sport.js
@@ -2,7 +2,6 @@ import carsData from "@/data/dataCar";
 import CarsList from "@/components/templates/CarsList";
 const Sport = () => {
     const sportcar = carsData.filter(car => car.category === 'sport');
-    console.log(sportcar);
     return (
         <div>
             <CarsList data={ sportcar } />
