c73cc3bf944d73bd65404572256f05bacc7c3e3b
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-02-06 15:04:09 +0900

    categories route

--- components/module/Categories.js
+++ components/module/Categories.js
@@ -1,31 +1,39 @@
-import styles from './Categories.module.css';
-import Link from 'next/link';
-import Suv from '../icons/Suv';
-import Sedan from '../icons/Sedan';
-import Hatchback from '../icons/Hatchback';
-import Sport from '../icons/Sport';
+import styles from "./Categories.module.css";
+import Link from "next/link";
+import Suv from "../icons/Suv";
+import Sedan from "../icons/Sedan";
+import Hatchback from "../icons/Hatchback";
+import Sport from "../icons/Sport";
 
 const Categories = () => {
-    return (
-        <div className={styles.container}>
-            <div>
-                <p>Sedan</p>
-                <Sedan/>
-            </div>
-            <div>
-                <p>SUV</p>
-                <Suv/>
-            </div>
-            <div>
-                <p>Hatchback</p>
-                <Hatchback/>
-            </div>
-            <div>
-                <p>Sport</p>
-                <Sport/>
-            </div>
+  return (
+    <div className={styles.container}>
+      <Link href="/categories/sedan">
+        <div>
+          <p>Sedan</p>
+          <Sedan />
         </div>
-    );
+      </Link>
+      <Link href="/categories/suv">
+        <div>
+          <p>SUV</p>
+          <Suv />
+        </div>
+      </Link>
+      <Link href="/categories/hatchback">
+        <div>
+          <p>Hatchback</p>
+          <Hatchback />
+        </div>
+      </Link>
+      <Link href="/categories/sport">
+        <div>
+          <p>Sport</p>
+          <Sport />
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Categories;
